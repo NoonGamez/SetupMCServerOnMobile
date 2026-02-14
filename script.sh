bb82d89b276a1e3921e7fbf5e3d0c770ded359ca
NoonGamez <80593203+NoonGamez@users.noreply.github.com>
2026-02-14 11:05:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,14 @@
 pkg update && pkg upgrade -y
 pkg install proot-distro wget -y
 proot-distro install ubuntu
-proot-distro login ubuntu
 
+proot-distro login ubuntu -- bash -c "
+    source /etc/profile
+    export PS1='[auto] \u@\h:\w\$ '
+    echo 'Ubuntu environment ready'
+    apt update -y && apt upgrade -y
+    apt install wget curl -y
+    apt install openjdk-25-jdk -y
+"
+proot-distro login ubuntu
 
